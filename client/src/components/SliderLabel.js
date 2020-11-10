1839140d6bc496a57d61e88f53d4ee463034b619
jshafto <jpshafto@gmail.com>
2020-11-10 11:48:44 -0500

    completed sorting drafts and deletion

--- client/src/components/SliderLabel.js
+++ client/src/components/SliderLabel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip'
 
 const ValueLabelComponent = ({ children, open, value }) => (
-    <Tooltip style={{ whiteSpace: 'pre-line' }} open={open} enterTouchDelay={0} placement="top" title={value}>
+    <Tooltip style={{ whiteSpace: 'pre-line' }} open={open} enterTouchDelay={0} placement="top" title={value} arrow>
         {children}
     </Tooltip>
 );
