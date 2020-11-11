ab0cbb37ed0c4afffe968c9929bd79653b773221
jshafto <jpshafto@gmail.com>
2020-11-11 13:22:50 -0500

    all routes for published works

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, User, Draft
+from app.models import db, User, Draft, Work
 from flask_login import current_user, login_required
 user_routes = Blueprint('users', __name__)
 
@@ -9,3 +9,10 @@ user_routes = Blueprint('users', __name__)
 def index():
     response = User.query.all()
     return {"users": [user.to_dict() for user in response]}
+
+
+@user_routes.route('/me/works')
+@login_required
+def get__own_works():
+    response = Work.query.filter_by(user_id=current_user.id).all()
+    return {work.id: work.work_info() for work in response}
